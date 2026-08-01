c1d45a7e0d951284c911b12668e1f749f3bb68d4
Dener <denerpneto@hotmail.com>
2026-08-01 14:31:50 -0300

    Adiciona tag do Google Analytics G-K610FW1QJX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,38 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
-export const metadata = {
-    title: "Extrator Contabil IA",
-    description: "Conversor de extratos bancarios em PDF para Excel",
-    verification: {
-        google: "vWijEHMJCwytd0S62ghqArfYi-Hnixw9qUVPISIczYE",
-    },
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Extrator Contábil IA",
+    description: "Seu extrator de extratos bancários com IA",
 };
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
     return (
         <html lang="pt-BR">
-            <body>{props.children}</body>
+            <head>
+                {/* Google tag (gtag.js) */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-K610FW1QJX"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-K610FW1QJX');
+          `}
+                </Script>
+            </head>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }
